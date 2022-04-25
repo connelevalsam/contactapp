ad1674f57f8e710b42b4e1e334c1a50228697934
connel evalsam <connelblaze@gmail.com>
2022-04-25 15:17:03 +0100

    added delete

--- components/DisplayScreen.js
+++ components/DisplayScreen.js
@@ -1,13 +1,21 @@
-const DisplayScreen = ({ contacts }) => {
+
+const DisplayScreen = ({ contacts, delContact }) => {
     console.log(contacts)
     return ( 
         <div className="px-16 bg-gray-100 md:col-span-2 h-screen">
             <h1 className="font-bold text-slate-800">All Contacts</h1>
             {
                 contacts.map(contact => (
-                    <div key={contact.id} className="bg-white rounded-md overflow-hidden shadow p-3 my-3">
-                        <h1>{ contact.firstname } { contact.lastname }</h1>
-                        <span className="text-sm font-bold">{ contact.email }</span>
+                    <div key={contact.id} className="bg-white rounded-md overflow-hidden shadow p-3 my-3 grid grid-cols-3 gap-3">
+                        <div className="md:col-span-2">
+                            <h1>{ contact.firstname } { contact.lastname }</h1>
+                            <span className="text-sm font-bold">{ contact.email }</span>
+                        </div>
+                        <div className="md:col-span-1">
+                            <button className="bg-red-500 text-white p-2 rounded-md hover:scale-125 hover:opacity-80" onClick={
+                                () => delContact(contact)
+                            }>Del</button>
+                        </div>
                     </div>
                 ))
             }
--- pages/api/deleteContact.js
+++ pages/api/deleteContact.js
@@ -7,16 +7,19 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
       }
-    
       try {
 
-        const conatactData = req.body //JSON.parse(req.body);
-        
-        const saveContact = "Contacts "+ conatactData
+        const { id } = req.body //JSON.parse(req.body);
+        // console.log(req.body)
+        const delContact = await prisma.contact.delete({
+            where: {
+              id: id,
+            },
+        })
 
-        res.status(200).json(saveContact)
+        res.status(200).json(delContact)
     } catch (err) {
         console.log("from API error", err)
-        res.status(400).json({ message: 'Something went wrong' });
+        res.status(400).json({ message: 'Could not delete contact' });
     }
   }
